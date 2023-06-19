a36019bb63946711438c60f8424decd1e518c015
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-06-19 18:08:27 -0400

    rewrite make file tests by file

---
+++ tests/testthat/test_readme.R
@@ -0,0 +1,15 @@
+test_that("make readme works", {
+
+  dir <- fs::file_temp(pattern = "testproj")
+
+  create_local_study(dir = dir)
+
+  makeReadMe(projectPath = dir, open = FALSE)
+
+  expect_proj_file("README.md")
+  expect_snapshot(writeLines(read_utf8(proj_path("README.md"))),
+                  transform = scrub_testpkg)
+
+
+
+})
