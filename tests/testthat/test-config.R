27e763478640d561aa0efdbd03d0607fcb361e81
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2023-06-22 17:50:10 -0400

    update config test

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -7,9 +7,6 @@ test_that("makeConfig works", {
   makeConfig(block = "test", database = "synpuf_110k")
 
   expect_proj_file("config.yml")
-  expect_snapshot(writeLines(read_utf8(proj_path("config.yml"))),
-                  transform = scrub_testpkg)
-
 
 
 })
