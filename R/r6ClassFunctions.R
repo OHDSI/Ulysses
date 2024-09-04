9b1885f31a4380eb197219dda8a4da8c2183c7f5
Lavallee,Dr.,Martin (MED MA) BIP-US-R <martin.lavallee@boehringer-ingelheim.com>
2024-09-04 12:58:50 -0400

    separate out r6 classes

--- R/r6ClassFunctions.R
+++ R/r6ClassFunctions.R
@@ -12,6 +12,13 @@
   invisible(private)
 }
 
+.setNumeric <- function(private, key, value) {
+  checkmate::assert_numeric(x = value, null.ok = FALSE)
+  private[[key]] <- value
+  invisible(private)
+}
+
+
 .setClass <- function(private, key, value, class, nullable = FALSE) {
   checkmate::assert_class(x = value, classes = class, null.ok = nullable)
   private[[key]] <- value
@@ -29,17 +36,22 @@
   invisible(private)
 }
 
-
-
-.setActiveString <- function(private, key, value, idx) {
-  # return the value if nothing added
-  if(missing(value)) {
-    vv <- private[[key]][idx]
-    return(vv)
-  }
-  # replace the value if value assigned
-  .setString(private = private, key = key, value = value, idx = idx)
+.setTimeStamp <- function(private, key, value) {
+  checkmate::assert_posixct(x = value)
+  private[[key]] <- value
+  invisible(private)
 }
+
+#
+# .setActiveString <- function(private, key, value, idx) {
+#   # return the value if nothing added
+#   if(missing(value)) {
+#     vv <- private[[key]][idx]
+#     return(vv)
+#   }
+#   # replace the value if value assigned
+#   .setString(private = private, key = key, value = value, idx = idx)
+# }
 
 
 
@@ -64,3 +76,20 @@
   # replace the value if value assigned
   .setInteger(private = private, key = key, value = value)
 }
+
+
+# .setGeneratedCohorts <- function(private, key = ".generatedCohorts", value) {
+#   # return the value if nothing added
+#   if(missing(value)) {
+#     vv <- private[[key]]
+#     if (is.null(vv)) {
+#       invisible(NULL)
+#     } else {
+#       genCohorts <- vv$showTable()
+#       return(genCohorts)
+#     }
+#
+#   }
+#   # replace the value if value assigned
+#   .setClass(private = private, key = key, class = "GeneratedCohorts", value = value)
+# }
